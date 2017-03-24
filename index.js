014364d6d30b3110887717a095d10269d096e323
Mike Mechanic <titusrecords@hotmail.com>
2017-03-24 22:59:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array){
+  index = 0
+  array.forEach(element =>{
+    console.log(`${index}: ${element}`)
+    index++
+  })
+}
+
+function iterate(callBack){
+  var array = [2, 3, 4]
+  callBack(array)
+  return array
+}
+
+function doToArray (array, callback){
+  callback(array.forEach)
+}
